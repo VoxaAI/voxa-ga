23c612746d00a0c446395de2546a884e1a9439fd
Rommel Berrios <rommel.berrios93@gmail.com>
2020-02-26 12:16:57 -0600

    remove health check event

--- tests/voxa-ga.spec.js
+++ tests/voxa-ga.spec.js
@@ -9,9 +9,13 @@ const { expect } = chai;
 const voxaGA = require("../lib/voxa-ga");
 
 const alexaEvent = {
-  user: { userId: "blah" },
+  user: {
+    userId: "blah"
+  },
   session: {
-    user: { userId: "blah" }
+    user: {
+      userId: "blah"
+    }
   },
   request: {
     type: "IntentRequest",
@@ -19,9 +23,18 @@ const alexaEvent = {
     intent: {
       name: "BlahIntent",
       slots: {
-        SlotA: { name: "SlotA", value: 10 },
-        SlotB: { name: "SlotB", value: 11 },
-        SlotC: { name: "SlotC", value: "a" }
+        SlotA: {
+          name: "SlotA",
+          value: 10
+        },
+        SlotB: {
+          name: "SlotB",
+          value: 11
+        },
+        SlotC: {
+          name: "SlotC",
+          value: "a"
+        }
       }
     }
   }
@@ -31,16 +44,22 @@ describe("voxa-ga.spec.js", () => {
   let skill;
   let alexaPlatform;
   let rider;
-
+  let sentFn;
   beforeEach(() => {
-    skill = new VoxaApp({ views: {} });
+    skill = new VoxaApp({
+      views: {}
+    });
     alexaPlatform = new AlexaPlatform(skill);
 
     skill.onIntentRequest(event => {
       rider = event.ga;
+
+      sentFn = simple.mock(rider.visitor, "send").callbackWith(null, "sent");
     });
 
-    skill.onIntent("BlahIntent", () => ({ to: "die" }));
+    skill.onIntent("BlahIntent", () => ({
+      to: "die"
+    }));
   });
 
   afterEach(() => {
@@ -48,7 +67,10 @@ describe("voxa-ga.spec.js", () => {
   });
 
   it("logs slot values", () => {
-    voxaGA(skill, { trackingId: "blah", suppressSending: true });
+    voxaGA(skill, {
+      trackingId: "blah",
+      suppressSending: true
+    });
 
     return alexaPlatform.execute(alexaEvent, {}).then(() => {
       const queue = rider.visitor._queue;
@@ -81,6 +103,35 @@ describe("voxa-ga.spec.js", () => {
     });
   });
 
+  it("grab platform specificKey", () => {
+    voxaGA(skill, {
+      alexa: "blah",
+      suppressSending: false
+    });
+
+    return alexaPlatform.execute(alexaEvent, {}).then(() => {
+      const queue = rider.visitor._queue;
+      expect(queue).to.have.lengthOf(7);
+      expect(sentFn.callCount).to.be.equal(1);
+    });
+  });
+
+  it("does not log health check events", () => {
+    const healthCheckEvent = _.cloneDeep(alexaEvent);
+    _.set(healthCheckEvent, "request.intent.slots.is_health_check", {
+      name: "is_health_check",
+      value: true
+    });
+    voxaGA(skill, {
+      trackingId: "blah",
+      suppressSending: false
+    });
+
+    return alexaPlatform.execute(healthCheckEvent, {}).then(() => {
+      expect(sentFn.callCount).to.be.equal(0);
+    });
+  });
+
   it("does not log suppressed slots", () => {
     voxaGA(skill, {
       trackingId: "blah",
@@ -99,8 +150,13 @@ describe("voxa-ga.spec.js", () => {
           ev: 10
         })
       ).to.exist;
-      expect(_.find(queue, { t: "event", ec: "Params", ea: "SlotB" })).to.not
-        .exist;
+      expect(
+        _.find(queue, {
+          t: "event",
+          ec: "Params",
+          ea: "SlotB"
+        })
+      ).to.not.exist;
     });
   });
 });
